68c49ff9d51a31627b2226027a2b43479e88b48c
ranko r sredojevic <rrs@berkeley.edu>
2017-10-31 20:20:51 -0700

    Finish HW2
    I found that:
    
      - high LR is useful (faster training) if it can be supported
      - add LR coolding schedule to enable increased early RL but still converge
      - baseline is useful, usually enables cutting the simulation batch to ~1/2
      - starting from smaller (more constrained) networks seems to give more intuitive results
      - using larger networks right away seems to get training confused and performs poorly

---
+++ hw2/invpend.sh
@@ -0,0 +1,1 @@
+python train_pg.py InvertedPendulum-v1 -n 150 -b 7500 -bl -e 10 -rtg -lr 0.008 --exp_name invpend --render --stepping_lr 20 --step_factor 0.5
--- hw2/runner_sec4.2.sh
+++
@@ -1,1 +0,0 @@
-python train_pg.py InvertedPendulum-v1 -n 150 -b 15000 -e 10 -rtg -lr 0.008 --exp_name run_n150_b15k_rtg_lr08
