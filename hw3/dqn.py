Fix Bellman error expression (diffing is done through backprop).

hw3/dqn.py
@@ -15,7 +15,7 @@ def report(tag, tsr):
 
 def q_vals(q, act, num_actions):
     act_idx = tf.one_hot(act, depth = num_actions)
-    return tf.multiply(q, act_idx)
+    return tf.reduce_sum(tf.multiply(q, act_idx), axis=1)
 
 def learn(env,
           q_func,
@@ -165,7 +165,8 @@ def learn(env,
     report("y_i", y_i)
     report("act_t_ph", act_t_ph)
 
-    total_error = tf.reduce_mean(tf.reduce_sum(q_vals(q_phi, act_t_ph, num_actions)) - y_i)
+    diffs = q_vals(q_phi, act_t_ph, num_actions) - tf.stop_gradient(y_i)
+    total_error = 0.5 * tf.square(diffs)
     report("total_error", total_error)
     
     ######

hw3/dqn_utils.py
@@ -7,11 +7,9 @@ import random
 
 def huber_loss(x, delta=1.0):
     # https://en.wikipedia.org/wiki/Huber_loss
-    return tf.select(
-        tf.abs(x) < delta,
-        tf.square(x) * 0.5,
-        delta * (tf.abs(x) - 0.5 * delta)
-    )
+    return tf.select(tf.abs(x) < delta,
+                     tf.square(x) * 0.5,
+                     delta * (tf.abs(x) - 0.5 * delta))
 
 def sample_n_unique(sampling_f, n):
     """Helper function. Given a function `sampling_f` that returns